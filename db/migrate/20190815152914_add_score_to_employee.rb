2fc325bb159b592a5b0d258aa96a4fcef371d9a0
Ashwini Sukale <ashwinisukale16@gmail.com>
2019-08-15 21:16:46 +0530

    Add default seed data

--- db/migrate/20190815152914_add_score_to_employee.rb
+++ db/migrate/20190815152914_add_score_to_employee.rb
@@ -1,5 +1,5 @@
 class AddScoreToEmployee < ActiveRecord::Migration[5.2]
   def change
-    add_column :employees, :score, :float
+    add_column :employees, :score, :float, default: 0
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command { name: or created alongside the database with db:setup}.
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create{ name: [{ name:  name: 'Star Wars' }, { name:  name: 'Lord of the Rings' }]}
+#   Character.create{ name: name: 'Luke', movie: movies.first}
+Employee.destroy_all
+
+Employee.create!([{ name: 'Ashwini',referral_id:  nil},
+{ name: 'Rahul', referral_id: 1},
+{ name: 'Sandip',referral_id:  1},
+{ name: 'Jagdish',referral_id:  2},
+{ name: 'Teena', referral_id: 2},
+{ name: 'Meena', referral_id: 3},
+{ name: 'Harry', referral_id: 4},
+{ name: 'Ram',referral_id:  4},
+{ name: 'Sebin', referral_id: 5},
+{ name: 'Jerry',referral_id:  5},
+{ name: 'Rohini',referral_id:  6},
+{ name: 'Sweta', referral_id: 6},
+{ name: 'Tom', referral_id: 5},
+{ name: 'Vikas', referral_id: 13},
+{ name: 'Maithali', referral_id: 13}])
+
+p "Created #{Employee.count} Employees "
